3f82f9bae7f3e27d1eda8706d90ddaad96ca4c0b
robertouski <rmoncayoandrade@gmail.com>
2024-07-30 18:36:00 -0500

    FEAT: models

---
+++ backend/src/models/user.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { randomUUID } from "crypto";
+
+const { Schema } = mongoose;
+const chatSchema = new Schema({
+  id: {
+    type: String,
+    default: randomUUID(),
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    require: true,
+  },
+});
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+    minlength: [3, "Username must be at least 3 characters long"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
+  chats: [chatSchema],
+});
+
+export default mongoose.model("User", userSchema);
