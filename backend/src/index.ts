892064cb142edebafaf6e7ba1b458f2d51b4b7f1
robertouski <rmoncayoandrade@gmail.com>
2024-07-30 18:39:24 -0500

    CHORE: Minor tweaks to backend configuration and structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import app from "./app.js";
 import connectToDatabase from "./db/connection.js";
 
 const PORT = process.env.PORT || 5000
+console.log("Here is the PORT", PORT)
 connectToDatabase()
   .then(() => {
     app.listen(PORT, () => console.log("Server Open & connected to DB"));
