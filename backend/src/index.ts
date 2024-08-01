6423f615d3d8b3ab24d28f89cb75140f8ab34a99
robertouski <rmoncayoandrade@gmail.com>
2024-07-31 20:46:55 -0500

    Irrelevant changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,8 @@ import app from "./app.js";
 import connectToDatabase from "./db/connection.js";
 
 const PORT = process.env.PORT || 5000
-console.log("Here is the PORT", PORT)
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log("Server Open & connected to DB"));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT} & connected to DB`));
   })
   .catch((err) => console.log(err));
