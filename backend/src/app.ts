358528770126ef3c2df3ebc8f9267d16f43e48aa
robertouski <rmoncayoandrade@gmail.com>
2024-12-03 18:48:09 -0500

    Backend add/changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,14 @@ import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const COOKIE_SECRET = process.env.COOKIE_SECRET || "secret"; 
 config();
 
 const app = express();
 
+app.use(cors({origin: "http://localhost:5173", credentials: true}))
 app.use(express.json());
 app.use(cookieParser(COOKIE_SECRET))
 
